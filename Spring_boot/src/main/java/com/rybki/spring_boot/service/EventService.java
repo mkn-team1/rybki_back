4da96a699f482df5daa3d2f5c2e63618857894b8
Vazgen Soghoyan <sogvazulchik@gmail.com>
2025-10-12 11:30:22 +0300

    EventService (without Redis)

--- Spring_boot/src/main/java/com/rybki/spring_boot/service/EventService.java
+++ Spring_boot/src/main/java/com/rybki/spring_boot/service/EventService.java
@@ -1,4 +1,25 @@
 package com.rybki.spring_boot.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
 public class EventService {
+
+    public String createEvent(String creatorId) {
+        String eventId = java.util.UUID.randomUUID().toString();
+        log.info("Creating new event: eventId={}, creatorId={}", eventId, creatorId);
+
+        // TODO: записать в Redis participants и т.п.
+
+        return eventId;
+    }
+
+    public void endEvent(String eventId) {
+        log.info("Ending event: eventId={}", eventId);
+
+        // TODO: собрать все идеи, удалить ключи Redis, уведомить участников
+
+    }
 }
